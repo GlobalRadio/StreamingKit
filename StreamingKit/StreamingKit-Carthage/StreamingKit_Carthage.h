88b1d05495330231e68489ae4c6f4ccfc6349c09
Samuel Rayment <sam.rayment@thisisglobal.com>
2018-02-21 15:32:39 +0000

    Adds a carthage build target

---
+++ StreamingKit/StreamingKit-Carthage/StreamingKit_Carthage.h
@@ -0,0 +1,19 @@
+//
+//  StreamingKit_Carthage.h
+//  StreamingKit-Carthage
+//
+//  Created by Sam Rayment on 21/02/2018.
+//  Copyright © 2018 Thong Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StreamingKit_Carthage.
+FOUNDATION_EXPORT double StreamingKit_CarthageVersionNumber;
+
+//! Project version string for StreamingKit_Carthage.
+FOUNDATION_EXPORT const unsigned char StreamingKit_CarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StreamingKit_Carthage/PublicHeader.h>
+
+
